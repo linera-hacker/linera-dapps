13d8e2119fafc01bc26ffd47ee25d849827e2ba6
linera-hacker <zzhymcfjx@hotmail.com>
2024-10-08 19:51:38 +0800

    Add swap application

---
+++ swap/src/service.rs
@@ -0,0 +1,36 @@
+#![cfg_attr(target_arch = "wasm32", no_main)]
+
+mod state;
+
+use self::state::Application;
+use linera_sdk::{
+    base::WithServiceAbi,
+    views::{View, ViewStorageContext},
+    Service, ServiceRuntime,
+};
+
+pub struct ApplicationService {
+    state: Application,
+    runtime: ServiceRuntime<Self>,
+}
+
+linera_sdk::service!(ApplicationService);
+
+impl WithServiceAbi for ApplicationService {
+    type Abi = swap::ApplicationAbi;
+}
+
+impl Service for ApplicationService {
+    type Parameters = ();
+
+    async fn new(runtime: ServiceRuntime<Self>) -> Self {
+        let state = Application::load(runtime.root_view_storage_context())
+            .await
+            .expect("Failed to load state");
+        ApplicationService { state, runtime }
+    }
+
+    async fn handle_query(&self, _query: Self::Query) -> Self::QueryResponse {
+        panic!("Queries not supported by application");
+    }
+}
