fa33ca5b87c3eb3c19f496f84b98b081fc46054b
linera-hacker <zzhymcfjx@hotmail.com>
2024-10-12 12:24:22 +0800

    Support erc20

--- deploy-local.sh
+++ deploy-local.sh
@@ -41,24 +41,32 @@ function run_service () {
 create_wallet 10
 create_wallet 11
 create_wallet 12
+create_wallet 13
 
 print $'\U01F4AB' $YELLOW " Deploying ERC20 application ..."
-erc20_bid=`linera --with-wallet 10 publish-bytecode ./target/wasm32-unknown-unknown/release/erc20_{contract,service}.wasm`
-erc20_appid=`linera --with-wallet 10 create-application $erc20_bid --json-argument '{"initial_supply":"20","name":"TestToken","symbol":"TKA","decimals":18,"initial_currency":"1", "fixed_currency":true, "fee_rate": "1"}'`
+erc20_1_bid=`linera --with-wallet 10 publish-bytecode ./target/wasm32-unknown-unknown/release/erc20_{contract,service}.wasm`
+erc20_1_appid=`linera --with-wallet 10 create-application $erc20_bid --json-argument '{"initial_supply":"20000000","name":"Test Linera ERC20 Token","symbol":"TLA","decimals":18,"initial_currency":"0.00001", "fixed_currency":false, "fee_rate": "1"}'`
 print $'\U01f499' $LIGHTGREEN " ERC20 application deployed"
-echo -e "    Bytecode ID:    $BLUE$erc20_bid$NC"
-echo -e "    Application ID: $BLUE$erc20_appid$NC"
+echo -e "    Bytecode ID:    $BLUE$erc20_1_bid$NC"
+echo -e "    Application ID: $BLUE$erc20_1_appid$NC"
+
+print $'\U01F4AB' $YELLOW " Deploying WTLINERA application ..."
+erc20_2_bid=`linera --with-wallet 11 publish-bytecode ./target/wasm32-unknown-unknown/release/erc20_{contract,service}.wasm`
+erc20_2_appid=`linera --with-wallet 11 create-application $erc20_bid --json-argument '{"initial_supply":"20000000","name":"Wrapper Testnet LINERA Token","symbol":"WTLINERA","decimals":18,"initial_currency":"1", "fixed_currency":true, "fee_rate": "1"}'`
+print $'\U01f499' $LIGHTGREEN " WLINERA application deployed"
+echo -e "    Bytecode ID:    $BLUE$erc20_2_bid$NC"
+echo -e "    Application ID: $BLUE$erc20_2_appid$NC"
 
 print $'\U01F4AB' $YELLOW " Deploying Swap Pool application ..."
-swap_pool_bid=`linera --with-wallet 11 publish-bytecode ./target/wasm32-unknown-unknown/release/swap_pool_{contract,service}.wasm`
-swap_pool_appid=`linera --with-wallet 11 create-application $swap_pool_bid`
+swap_pool_bid=`linera --with-wallet 12 publish-bytecode ./target/wasm32-unknown-unknown/release/swap_pool_{contract,service}.wasm`
+swap_pool_appid=`linera --with-wallet 12 create-application $swap_pool_bid`
 print $'\U01f499' $LIGHTGREEN " Swap Pool application deployed"
 echo -e "    Bytecode ID:    $BLUE$swap_pool_bid$NC"
 echo -e "    Application ID: $BLUE$swap_pool_appid$NC"
 
 print $'\U01F4AB' $YELLOW " Deploying Swap Router application ..."
-swap_router_bid=`linera --with-wallet 12 publish-bytecode ./target/wasm32-unknown-unknown/release/swap_router_{contract,service}.wasm`
-swap_router_appid=`linera --with-wallet 12 create-application $swap_router_bid \
+swap_router_bid=`linera --with-wallet 13 publish-bytecode ./target/wasm32-unknown-unknown/release/swap_router_{contract,service}.wasm`
+swap_router_appid=`linera --with-wallet 13 create-application $swap_router_bid \
     --json-parameters "{\"pool_application_id\":\"$swap_pool_appid\"}"`
 print $'\U01f499' $LIGHTGREEN " Swap Router application deployed"
 echo -e "    Bytecode ID:    $BLUE$swap_router_bid$NC"
@@ -67,5 +75,6 @@ echo -e "    Application ID: $BLUE$swap_router_appid$NC"
 run_service 10 &
 run_service 11 &
 run_service 12 &
+run_service 13 &
 
 sleep 1000000
