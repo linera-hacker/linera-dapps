0405d6d7a2ad27f3cab31d8c5f9479ed9dccf829
linera-hacker <zzhymcfjx@hotmail.com>
2024-10-13 13:05:31 +0800

    Support error recover

--- deploy-local.sh
+++ deploy-local.sh
@@ -31,15 +31,19 @@ function create_wallet() {
   linera -w $1 wallet show
 }
 
+function __run_service() {
+  linera -w $1 service --port $2 --external-signing false
+  if [ ! $? -eq 0 ]; then
+    echo "Run with official release"
+    linera -w $1 service --port $2
+  fi
+}
+
 function run_service () {
   local_port=`expr 30080 + $1`
   pub_port=`expr 40100 + $1`
 
-  linera -w $1 service --port $local_port --external-signing false &
-  if [ ! $? -eq 0 ]; then
-    echo "Run with official release"
-    linera -w $1 service --port $local_port &
-  fi
+  __run_service $1 $local_port &
 
   sleep 10
   socat TCP4-LISTEN:$pub_port TCP4:localhost:$local_port
