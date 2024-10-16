728d26d3e07a211905d4b9721049eef7ab51a2cf
linera-hacker <zzhymcfjx@hotmail.com>
2024-10-16 20:29:07 +0800

    Merge pool to router

--- swap/router/src/lib.rs
+++ swap/router/src/lib.rs
@@ -1,7 +1,45 @@
+use linera_sdk::base::ParseAmountError;
 use spec::swap::RouterApplicationAbi;
 use thiserror::Error;
 
 pub type ApplicationAbi = RouterApplicationAbi;
+
+#[derive(Debug, Error)]
+#[allow(dead_code)]
+pub enum PoolError {
+    #[error("Invalid initial amount")]
+    InvalidInitialAmount,
+
+    #[error(transparent)]
+    ParseAmountError(#[from] ParseAmountError),
+
+    #[error(transparent)]
+    ViewError(#[from] linera_sdk::views::ViewError),
+
+    #[error("Already exists")]
+    AlreadyExists,
+
+    #[error("Invalid pool")]
+    InvalidPool,
+
+    #[error("Permission denied")]
+    PermissionDenied,
+
+    #[error("Not supported")]
+    NotSupported,
+
+    #[error("Insufficient funds")]
+    InsufficientFunds,
+
+    #[error("Invalid amount")]
+    InvalidAmount,
+
+    #[error("Insufficient liquidity")]
+    InsufficientLiquidity,
+
+    #[error("Broken K")]
+    BrokenK,
+}
 
 #[derive(Debug, Error)]
 #[allow(dead_code)]
@@ -14,4 +52,7 @@ pub enum RouterError {
 
     #[error("Invalid pool")]
     InvalidPool,
+
+    #[error(transparent)]
+    PoolError(#[from] PoolError),
 }
